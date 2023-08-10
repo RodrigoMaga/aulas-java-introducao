4b9cdde5472e9b8b52cbceeef3b10ce126155c23
RodrigoMaga <rgn.nunnes@gmail.com>
2023-08-10 18:56:28 -0300

    todo

--- src/main/java/org/example/domain/Student.java
+++ src/main/java/org/example/domain/Student.java
@@ -4,7 +4,7 @@ public class Student {
     public String name;
     public String email;
     public float grade;
-
+//todo: colocar nota em letra ABC
     public boolean isApproved() {
         System.out.println("Nota: " + grade);
         return grade >= 6;
